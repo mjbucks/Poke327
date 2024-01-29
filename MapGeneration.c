6740b1952f2c670df534ac366fabf209856b1c07
mrohrer <mrohrer@pyrite-n4.cs.iastate.edu>
2024-01-28 20:56:56 -0600

    Added mountain, grass, water terrain

--- MapGeneration.c
+++ MapGeneration.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define MAP_WIDTH 80
 #define MAP_HEIGHT 21
 
@@ -27,12 +28,79 @@ void fill_board_with_short_grass(){
     }
 }
 
+void place_water(){
+    int water_height = rand()%15;
+    int water_width = rand()%70;
+    int water_x = rand()%(MAP_WIDTH-water_width);
+    int water_y = rand()%(MAP_HEIGHT-water_height);
+    for(int i = water_y; i < water_height + water_y; i++){
+        for(int j = water_x; j < water_width + water_x; j++){
+            if(isBorder(i, j) == 0){
+                map[i][j] = '~';
+            }
+        }
+    }
+}
+
+void place_mountains(){
+    int mountain_height = rand()%15;
+    int mountain_width = rand()%70;
+    int water_x = rand()%(MAP_WIDTH-mountain_width);
+    int water_y = rand()%(MAP_HEIGHT-mountain_height);
+    for(int i = water_y; i < mountain_height + water_y; i++){
+        for(int j = water_x; j < mountain_width + water_x; j++){
+            if(isBorder(i, j) == 0){
+                map[i][j] = '%';
+            }
+        }
+    }
+}
+
+void place_tall_grass(){
+    int grass_height = rand()%15;
+    int grass_width = rand()%70;
+    int water_x = rand()%(MAP_WIDTH-grass_width);
+    int water_y = rand()%(MAP_HEIGHT-grass_height);
+    for(int i = water_y; i < grass_height + water_y; i++){
+        for(int j = water_x; j < grass_width + water_x; j++){
+            if(isBorder(i, j) == 0){
+                map[i][j] = ':';
+            }
+        }
+    }
+}
+
+void place_short_grass(){
+    int grass_height = rand()%10;
+    int grass_width = rand()%50;
+    int water_x = rand()%(MAP_WIDTH-grass_width);
+    int water_y = rand()%(MAP_HEIGHT-grass_height);
+    for(int i = water_y; i < grass_height + water_y; i++){
+        for(int j = water_x; j < grass_width + water_x; j++){
+            if(isBorder(i, j) == 0){
+                map[i][j] = '.';
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
+
     fill_board_with_short_grass();
+    place_tall_grass();
+    place_water();
+    place_mountains();
+    place_tall_grass();
+    place_short_grass();
+    place_water();
+    place_mountains();
+    place_tall_grass();
+
     for(int i = 0; i < MAP_HEIGHT; i++){
         for(int j = 0; j < MAP_WIDTH; j++){
-            printf("%d ", map[i][j]);
+            printf("%c ", map[i][j]);
         }
         printf("\n");
     }
